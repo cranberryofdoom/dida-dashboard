f3d08c5475b735323b07277d5b49ea7a53ff4420
Ashley Qian <ashley.qian@duke.edu>
2013-11-20 11:34:35 -0500

    fixing teams

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,19 @@
+class TeamsController < ApplicationController
+
+	def index
+		@teams = Team.all
+	end
+
+	def save
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+end
--- db/migrate/20131016224142_create_teams_users.rb
+++ db/migrate/20131016224142_create_teams_users.rb
@@ -1,6 +1,6 @@
 class CreateTeamsUsers < ActiveRecord::Migration
   def change
-    create_table :teams_users do |t|
+    create_table :teams_users, :id => false do |t|
     	t.integer :team_id, :user_id
     end
     add_index :teams_users, :team_id
