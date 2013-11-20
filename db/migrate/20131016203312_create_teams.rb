b405dc9309c8762d8dff0127100a3cb3d71cd3c8
yan2 <niou@wl-10-190-200-130.wireless.duke.edu>
2013-11-20 11:33:56 -0500

    fixing team user tables

--- db/migrate/20131016203312_create_teams.rb
+++ db/migrate/20131016203312_create_teams.rb
@@ -2,10 +2,7 @@ class CreateTeams < ActiveRecord::Migration
 	def change
 		create_table :teams do |t|
 			t.string :name
-			t.integer :project_manager_id, :project_mentor_id
 			t.timestamps
 		end
-		add_index :teams, :project_manager_id
-		add_index :teams, :project_mentor_id
 	end
 end
