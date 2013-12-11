7b759e61b1dc06e05903873ebe917df477cd22d2
yan2 <niou@TTS-120271ML.local>
2013-12-11 10:38:37 -0500

    added designers to db

--- app/assets/javascripts/angular/controllers/teams_ctrl.js
+++ app/assets/javascripts/angular/controllers/teams_ctrl.js
@@ -10,5 +10,33 @@ Dida.controller('TeamIndexController', function($scope, $http, Team, Designer, $
 				});
 			}
 		}
-	}
+	};
+
+    $scope.startCallback = function(event, ui, user) {
+        console.log('Dropped into something' + user.id);
+        $scope.draggedDesigner = user;
+    };
+
+
+    $scope.dropCallback = function(event, ui, team) {
+        console.log('Dropped into something');
+        console.log($scope.draggedDesigner);
+        console.log(team.id);
+
+        $http.post('teams/' + team.id + '/add_designer', {'user_id': $scope.draggedDesigner.id})
+        .success(function(){
+            for(i in team.users){
+                if(team.users[i].id !=  $scope.draggedDesigner.id) {
+                    team.users += $scope.draggedDesigner;
+                  console.log(team.users);
+                    
+                }
+                else {
+                  console.log('already agdded');
+              }
+          }
+          
+      });
+    };
+
 });
