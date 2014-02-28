eab6ba51a7302157adc372d721390aaa5d7916c9
yan2 <niou@wl-10-190-67-50.wireless.duke.edu>
2014-02-28 15:13:19 -0500

    delete api folder and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 DidaDashboard::Application.routes.draw do
   devise_for :users
 
-  namespace :api do
-    resources :teams
-    resources :users
-    resources :projects
-  end
-
   resources :projects do
     post "update_team"
     post "update_designer"
